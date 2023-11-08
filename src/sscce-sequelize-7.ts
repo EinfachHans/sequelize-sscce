3aaf5ca210aca11dd824f50d64aa33d70123e8bd
EinfachHans <hanskrywaa@web.de>
2023-11-08 17:45:47 +0100

    chore: add sscce

--- src/sscce-sequelize-7.ts
+++ src/sscce-sequelize-7.ts
@@ -1,7 +1,8 @@
-import { DataTypes, Model } from '@sequelize/core';
+import { CreationOptional, DataTypes, Model, NonAttribute } from '@sequelize/core';
 import { createSequelize7Instance } from '../setup/create-sequelize-instance';
 import { expect } from 'chai';
 import sinon from 'sinon';
+import { AfterDestroy, Attribute, HasMany, NotNull } from '@sequelize/core/decorators-legacy';
 
 // if your issue is dialect specific, remove the dialects you don't need to test on.
 export const testingOnDialects = new Set(['mssql', 'sqlite', 'mysql', 'mariadb', 'postgres', 'postgres-native']);
@@ -21,14 +22,35 @@ export async function run() {
     },
   });
 
-  class Foo extends Model {}
+  const onDestroySpy = sinon.spy();
 
-  Foo.init({
-    name: DataTypes.TEXT,
-  }, {
-    sequelize,
-    modelName: 'Foo',
-  });
+  class Child extends Model {
+    declare id: CreationOptional<number>;
+
+    @Attribute(DataTypes.INTEGER)
+    @NotNull()
+    declare parentId: number;
+
+    @AfterDestroy()
+    static async onAfterDestroy() {
+      onDestroySpy();
+    }
+
+    declare parent?: NonAttribute<Parent>;
+  }
+
+  class Parent extends Model {
+    declare id: CreationOptional<number>;
+
+    @HasMany(() => Child, {
+      foreignKey: 'parentId',
+      inverse: 'parent',
+      hooks: true
+    })
+    declare steps?: NonAttribute<Child[]>;
+  }
+
+  sequelize.addModels([Parent, Child])
 
   // You can use sinon and chai assertions directly in your SSCCE.
   const spy = sinon.spy();
@@ -36,6 +58,18 @@ export async function run() {
   await sequelize.sync({ force: true });
   expect(spy).to.have.been.called;
 
-  console.log(await Foo.create({ name: 'TS foo' }));
-  expect(await Foo.count()).to.equal(1);
+  // Create parent
+  const parent = await Parent.create({});
+
+  // Create childs
+  await Child.bulkCreate([{parentId: parent.id}, {parentId: parent.id}])
+
+  // Destroy the parent
+  await parent.destroy();
+
+  // Childs should be destroyed - Works ✅
+  expect(await Child.count()).to.equal(0)
+
+  // On Destroy Spy should have been called twice (one per each child) - Does not work ❌
+  expect(onDestroySpy).to.have.been.calledTwice;
 }
